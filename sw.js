1dc152aa5746ded7ef5614aa8e5eaa3c82b0dffb
basthiancasparod-cmyk <basthiancasparod@gmail.com>
2025-09-25 19:27:45 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // 1. CAMBIA LA VERSIÓN DEL CACHÉ.
 //    Cada vez que subas una nueva versión de tu app, deberás incrementar este número (v2, v3, etc.).
-const CACHE_NAME = 'dashsy-cache-v1.0.4'; 
+const CACHE_NAME = 'dashsy-cache-v1.0.5'; 
 
 const urlsToCache = [
   './',
@@ -57,3 +57,4 @@ self.addEventListener('message', event => {
     self.skipWaiting();
   }
 });
+
