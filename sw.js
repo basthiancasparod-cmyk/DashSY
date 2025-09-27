178bae537b89510cd8c5ac47864730d2af8921a9
basthiancasparod-cmyk <basthiancasparod@gmail.com>
2025-09-26 20:27:29 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 1. VERSIÓN DEL CACHÉ INCREMENTADA
-const CACHE_NAME = 'dashsy-cache-v1.1.5'; // <-- CAMBIO #1
+const CACHE_NAME = 'dashsy-cache-v1.1.6'; // <-- CAMBIO #1
 
 const urlsToCache = [
   './',
@@ -50,3 +50,4 @@ self.addEventListener('message', event => {
     self.skipWaiting();
   }
 });
+
